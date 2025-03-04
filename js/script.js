382bd97bdc6deab9bf5608b63bf58eced9085ee6
Khaitiyya <knaw04@gmail.com>
2025-03-04 07:34:39 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").ATTRIBUTE_NODE;
+onclick = () => {
+  navbarNav.classList.toggle("active");
+};
